750ce502958fec16f466d32a0bfb74022b8c5eee
Srija Dasari <sd23aec@herts.ac.uk>
2025-01-06 06:52:21 +0530

    visualization.R

---
+++ visualization.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(ggplot2)
+pdf("visualization.pdf")
+P1 <- read.csv("Food_Supply_kcal_Data.csv")
+Confirmed  <- P1$Confirmed
+Deaths  <- P1$Deaths
+P1_complete <- P1[complete.cases(P1),]
+ggplot(P1_complete,aes ( x = Confirmed , y = Deaths ))+geom_point()+geom_smooth( method = "lm", formula = 'y ~ x')+ggtitle("Correlation of Covid-19 Death ratio and Confirmed cases in the world")+theme(plot.title = element_text(hjust = 0.5))+labs(x = "Confirmed Cases" , y = "Death Ratio")
+ggplot(data= P1_complete,aes(x = Deaths))+geom_histogram(aes(y = ..density..), bins = 15, fill ="grey",color = "1")+stat_function(fun = dnorm,args = list(mean = mean(P1_complete$Deaths),sd = sd(P1_complete$Deaths)), col = "red")+ggtitle("Histogram of Death Ratio")+theme(plot.title = element_text(hjust = 0.5))+labs( x = "Deaths Ratio")
+dev.off()
+
+
+  
